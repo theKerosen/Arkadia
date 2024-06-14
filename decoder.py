cff6f1ab004905f904f943a59dd4da55e9e58591
theKerosen <reallytsx@gmail.com>
2024-06-13 21:30:11 -0300

    1.0.1 --file inline argument added

--- decoder.py
+++ decoder.py
@@ -1,7 +1,17 @@
+import sys
+import argparse
 from modules.itb import itb
 from modules.btf import btf
 
-i = input("Input file path: ")
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", type=str, help="input file")
+args = parser.parse_args()
+
+if not args.file:
+    print("No input file provided")
+    sys.exit(1)
+
+i = args.file
 s = i.rsplit(".", -1)
 
 b2, extension = itb(f"{s[0]}.png")
--- encoder.py
+++ encoder.py
@@ -1,7 +1,18 @@
+import sys
+import argparse
 from modules.ftb import ftb
 from modules.bti import bti
 
-i = input("Input file path: ")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", type=str, help="input file")
+args = parser.parse_args()
+
+if not args.file:
+    print("No input file provided")
+    sys.exit(1)
+
+i = args.file
 b = ftb(i)
 s = i.rsplit(".", -1)
 bti(b, f"{s[0]}.png", s[1])
